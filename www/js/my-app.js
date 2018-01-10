5c2e0d1b77a2ba9dc2e18847be2b9f24a860f94b
Andrei Dashkevich <andrew.daskhevich@yandex.ru>
2018-01-10 09:53:51 +0300

    add framework 7 v2

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,68 +1,34 @@
-// Initialize app
-var myApp = new Framework7();
-var userDetails = new UserDetails();
+var app = new Framework7({
+    root: '#app',
+    name: 'TravelWallet',
+    id: 'com.startappsoft.travelwallet',
+    version: '1.0.0',
+    panel: {
+        swipe: 'left',
+    },
 
-// If we need to use custom DOM library, let's save it to $$ variable:
-var $$ = Dom7;
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+    routes: [
+        {
+            path: '/about/',
+            url: 'about.html',
+        },
+    ],
+    // ... other parameters
 });
 
-myApp.addView('.sign-up-view');
+var mainView = app.views.create('.view-main');
 
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
-});
-
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-
-});
-
-myApp.onPageInit('phone_number', function (page) {
-    // Do something here for "about" page
-    $$('#submit-phone').on('click', function (e) {
-        mainView.router.load({pageName: 'about'})
-    });
-});
-
-myApp.onPageInit('login-screen', function (page) {
-    var pageContainer = $$(page.container);
-    pageContainer.find('.list-button').on('click', function () {
-        var phone = pageContainer.find('input[name="phone"]').val(),
-            buttons = [
-                {
-                    text: 'Is this phone number correct ?\n' + phone,
-                    label: true
-                },
-                {
-                    text: 'Yes',
-                    onClick: function () {
-                        myApp.alert('Cancel clicked');
-                    }
-                },
-                {
-                    text: 'Change'
-                }
-            ];
-
-        myApp.actions(buttons);
-
-
-        // Handle username and password
-        /*myApp.alert('Username: ' + username + ', Password: ' + password, function () {
-            mainView.back();
-        });*/
-    });
-});
-
-mainView.router.loadPage('login-screen.html');
+// This view will support all global routes + own additional routes
+var view2 = app.views.create('.sign-up-view', {
+    // These routes are only available in this view
+    routesAdd: [
+        {
+            path: '/blog/',
+            url: './pages/blog.html',
+        },
+        {
+            path: '/post/',
+            url: './pages/post.html',
+        },
+    ],
+})

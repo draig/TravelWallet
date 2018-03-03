e92c0f55e5d646c63c20ee6587d8e8fec7f8cb3b
Andrei Dashkevich <andrew.daskhevich@yandex.ru>
2018-03-03 08:39:38 +0300

    visual improvement

--- www/js/helpers.js
+++ www/js/helpers.js
@@ -9,3 +9,9 @@ Template7.registerHelper('format_amount', function (amount, options) {
     if (typeof amount === 'function') amount = amount.call(this);
     return (Math.round(amount * 100)/100).toFixed(2);
 });
+
+Template7.registerHelper('format_currency', function (id, options) {
+    //options.hash object: console.log(options.hash) -> {delimiter: ', '}
+    if (typeof id === 'function') id = id.call(this);
+    return service.currency.get(id).title;
+});
